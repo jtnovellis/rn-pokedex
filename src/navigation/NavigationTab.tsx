fea8791f6c4213fc4f12de9aa808cac9dc1b04a9
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-24 17:47:59 -0500

    Tab navigation icon styles

--- src/navigation/NavigationTab.tsx
+++ src/navigation/NavigationTab.tsx
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import FavoriteScreen from '../screens/FavoriteScreen';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
@@ -14,9 +16,43 @@ const Tab = createBottomTabNavigator<RootStackParamList>();
 export default function NavigationTab() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name='Home' component={HomeScreen} />
-      <Tab.Screen name='Profile' component={ProfileScreen} />
-      <Tab.Screen name='Favorite' component={FavoriteScreen} />
+      <Tab.Screen
+        name='Favorite'
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: 'Favorites',
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='heart' color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: () => renderPokeBall(),
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'My Account',
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='user' color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+}
+
+function renderPokeBall() {
+  return (
+    <Image
+      source={require('../../assets/pokeball.png')}
+      style={{ width: 65, height: 65, top: -15 }}
+    />
   );
 }
