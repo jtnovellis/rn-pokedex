9c876b52f1fcafd01d6c563e2ff11e3181812abe
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-26 18:52:42 -0500

    Login form, validations

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,9 +1,76 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Keyboard,
+  StyleSheet,
+  Button,
+} from 'react-native';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (values) => {
+      console.log('data', values);
+    },
+  });
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Log in</Text>
+      <TextInput
+        placeholder='Username'
+        style={styles.input}
+        autoCapitalize='none'
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue('username', text)}
+      />
+      <TextInput
+        placeholder='Password'
+        style={styles.input}
+        autoCapitalize='none'
+        secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue('password', text)}
+      />
+      <Button title='Sign in' onPress={() => formik.handleSubmit()} />
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   );
 }
+
+function validationSchema() {
+  return {
+    username: Yup.string().required('The user is required'),
+    password: Yup.string().required('The password is required'),
+  };
+}
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+  error: {
+    textAlign: 'center',
+    color: '#f00',
+    marginTop: 20,
+  },
+});
